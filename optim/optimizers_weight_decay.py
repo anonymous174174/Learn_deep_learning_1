84b81897d4dd55d3e03b8673cbfec3d3c23b4401
Chaitanya Chaniyara <54537142+anonymous174174@users.noreply.github.com>
2025-05-16 11:56:25 +0000

    fix

--- optim/optimizers_weight_decay.py
+++ optim/optimizers_weight_decay.py
@@ -1,31 +1,30 @@
 from abc import ABC, abstractmethod
 import torch
+
 class Optimizer(ABC):
     """Abstract base class for all optimizers"""
     def __init__(self, layers, lr, weight_decay=0.0):
         self.layers = layers
         self.lr = lr
-        self.weight_decay = weight_decay  # Add weight decay parameter
-    
+        self.weight_decay = weight_decay
+
     @abstractmethod
     def step(self):
         pass
 
 class SGD(Optimizer):
-    """Stochastic Gradient Descent with Weight Decay"""
     def __init__(self, layers, lr, weight_decay=0.0):
         super().__init__(layers, lr, weight_decay)
-        
+
     def step(self):
         with torch.no_grad():
             for layer in self.layers:
-                # Update weights with weight decay
-                layer.weight.data -= self.lr * (layer.weight.grad + self.weight_decay * layer.weight.data)
-                # Update biases (no weight decay)
+                if self.weight_decay > 0:
+                    layer.weight.data *= (1 - self.lr * self.weight_decay)
+                layer.weight.data -= self.lr * layer.weight.grad
                 layer.bias.data -= self.lr * layer.bias.grad
 
 class Momentum(Optimizer):
-    """SGD with Momentum and Weight Decay"""
     def __init__(self, layers, lr, beta=0.9, weight_decay=0.0):
         super().__init__(layers, lr, weight_decay)
         self.beta = beta
@@ -45,17 +44,12 @@ class Momentum(Optimizer):
                     self.beta * self.velocities[i]['bias'] +
                     (1 - self.beta) * layer.bias.grad
                 )
-                # Update weights with weight decay
-                layer.weight.data -= self.lr * (
-                    self.velocities[i]['weights'] + self.weight_decay * layer.weight.data
-                )
-                # Update biases (no weight decay)
+                if self.weight_decay > 0:
+                    layer.weight.data *= (1 - self.lr * self.weight_decay)
+                layer.weight.data -= self.lr * self.velocities[i]['weights']
                 layer.bias.data -= self.lr * self.velocities[i]['bias']
+
 class Nesterov(Optimizer):
-    """Nesterov Momentum with Weight Decay
-    Uses a reformulated version of the standard nesterov equation
-    v_t = β·v_{t-1} + (1-β)·g_t
-    θ_{t+1} = θ_t - η·(β·v_t + (1-β)·g_t)"""
     def __init__(self, layers, lr, beta=0.9, weight_decay=0.0):
         super().__init__(layers, lr, weight_decay)
         self.beta = beta
@@ -68,78 +62,24 @@ class Nesterov(Optimizer):
         with torch.no_grad():
             for i, layer in enumerate(self.layers):
                 self.velocities[i]['weights'] = (
-                    self.beta * self.velocities[i]['weights'] + 
+                    self.beta * self.velocities[i]['weights'] +
                     (1 - self.beta) * layer.weight.grad
                 )
                 self.velocities[i]['bias'] = (
-                    self.beta * self.velocities[i]['bias'] + 
+                    self.beta * self.velocities[i]['bias'] +
                     (1 - self.beta) * layer.bias.grad
                 )
-                # Update weights with weight decay
+                if self.weight_decay > 0:
+                    layer.weight.data *= (1 - self.lr * self.weight_decay)
                 layer.weight.data -= self.lr * (
                     self.beta * self.velocities[i]['weights'] +
-                    (1 - self.beta) * layer.weight.grad +
-                    self.weight_decay * layer.weight.data
+                    (1 - self.beta) * layer.weight.grad
                 )
-                # Update biases (no weight decay)
                 layer.bias.data -= self.lr * (
                     self.beta * self.velocities[i]['bias'] +
                     (1 - self.beta) * layer.bias.grad
                 )
-
-class Adam(Optimizer):
-    """Adam with Weight Decay"""
-    def __init__(self, layers, lr, beta1=0.9, beta2=0.999, epsilon=1e-8, weight_decay=0.0):
-        super().__init__(layers, lr, weight_decay)
-        self.beta1 = beta1
-        self.beta2 = beta2
-        self.epsilon = epsilon
-        self.m = [{
-            'weights': torch.zeros_like(layer.weight.data),
-            'bias': torch.zeros_like(layer.bias.data)
-        } for layer in layers]
-        self.v = [{
-            'weights': torch.zeros_like(layer.weight.data),
-            'bias': torch.zeros_like(layer.bias.data)
-        } for layer in layers]
-        self.t = 0
-
-    def step(self):
-        self.t += 1
-        with torch.no_grad():
-            for i, layer in enumerate(self.layers):
-                self.m[i]['weights'] = (
-                    self.beta1 * self.m[i]['weights'] +
-                    (1 - self.beta1) * layer.weight.grad
-                )
-                self.m[i]['bias'] = (
-                    self.beta1 * self.m[i]['bias'] +
-                    (1 - self.beta1) * layer.bias.grad
-                )
-                self.v[i]['weights'] = (
-                    self.beta2 * self.v[i]['weights'] +
-                    (1 - self.beta2) * torch.square(layer.weight.grad)
-                )
-                self.v[i]['bias'] = (
-                    self.beta2 * self.v[i]['bias'] +
-                    (1 - self.beta2) * torch.square(layer.bias.grad)
-                )
-
-                m_hat_w = self.m[i]['weights'] / (1 - self.beta1**self.t)
-                m_hat_b = self.m[i]['bias'] / (1 - self.beta1**self.t)
-                v_hat_w = self.v[i]['weights'] / (1 - self.beta2**self.t)
-                v_hat_b = self.v[i]['bias'] / (1 - self.beta2**self.t)
-
-                # Update weights with weight decay
-                layer.weight.data -= self.lr * (
-                    m_hat_w / (torch.sqrt(v_hat_w) + self.epsilon +
-                    self.weight_decay * layer.weight.data
-                ))
-                # Update biases (no weight decay)
-                layer.bias.data -= self.lr * m_hat_b / (torch.sqrt(v_hat_b) + self.epsilon)
-
 class RMSprop(Optimizer):
-    """RMSprop with Weight Decay"""
     def __init__(self, layers, lr, beta=0.9, epsilon=1e-8, weight_decay=0.0):
         super().__init__(layers, lr, weight_decay)
         self.beta = beta
@@ -160,22 +100,18 @@ class RMSprop(Optimizer):
                     self.beta * self.s[i]['bias'] +
                     (1 - self.beta) * torch.square(layer.bias.grad)
                 )
-                # Update weights with weight decay
-                layer.weight.data -= self.lr * (
-                    layer.weight.grad / (torch.sqrt(self.s[i]['weights']) + self.epsilon +
-                    self.weight_decay * layer.weight.data
-                ))
-                # Update biases (no weight decay)
+                if self.weight_decay > 0:
+                    layer.weight.data *= (1 - self.lr * self.weight_decay)
+                layer.weight.data -= self.lr * layer.weight.grad / (torch.sqrt(self.s[i]['weights']) + self.epsilon)
                 layer.bias.data -= self.lr * layer.bias.grad / (torch.sqrt(self.s[i]['bias']) + self.epsilon)
 
-
-class Nadam(Optimizer):
-    """Nadam (Adam with Nesterov Momentum) with Weight Decay"""
+class Adam(Optimizer):
     def __init__(self, layers, lr, beta1=0.9, beta2=0.999, epsilon=1e-8, weight_decay=0.0):
         super().__init__(layers, lr, weight_decay)
         self.beta1 = beta1
         self.beta2 = beta2
         self.epsilon = epsilon
+        self.t = 0
         self.m = [{
             'weights': torch.zeros_like(layer.weight.data),
             'bias': torch.zeros_like(layer.bias.data)
@@ -184,12 +120,12 @@ class Nadam(Optimizer):
             'weights': torch.zeros_like(layer.weight.data),
             'bias': torch.zeros_like(layer.bias.data)
         } for layer in layers]
-        self.t = 0
 
     def step(self):
         self.t += 1
         with torch.no_grad():
             for i, layer in enumerate(self.layers):
+                # Update moments
                 self.m[i]['weights'] = (
                     self.beta1 * self.m[i]['weights'] +
                     (1 - self.beta1) * layer.weight.grad
@@ -207,20 +143,60 @@ class Nadam(Optimizer):
                     (1 - self.beta2) * torch.square(layer.bias.grad)
                 )
 
-                m_hat_w = self.m[i]['weights'] / (1 - self.beta1**self.t)
-                m_hat_b = self.m[i]['bias'] / (1 - self.beta1**self.t)
-                m_nesterov_w = self.beta1 * m_hat_w + (1 - self.beta1) * layer.weight.grad / (1 - self.beta1**self.t)
-                m_nesterov_b = self.beta1 * m_hat_b + (1 - self.beta1) * layer.bias.grad / (1 - self.beta1**self.t)
-                v_hat_w = self.v[i]['weights'] / (1 - self.beta2**self.t)
-                v_hat_b = self.v[i]['bias'] / (1 - self.beta2**self.t)
+                # Bias correction
+                m_hat_w = self.m[i]['weights'] / (1 - self.beta1 ** self.t)
+                m_hat_b = self.m[i]['bias'] / (1 - self.beta1 ** self.t)
+                v_hat_w = self.v[i]['weights'] / (1 - self.beta2 ** self.t)
+                v_hat_b = self.v[i]['bias'] / (1 - self.beta2 ** self.t)
 
-                # Update weights with weight decay
-                layer.weight.data -= self.lr * (
-                    m_nesterov_w / (torch.sqrt(v_hat_w) + self.epsilon +
-                    self.weight_decay * layer.weight.data
-                ))
-                # Update biases (no weight decay)
-                layer.bias.data -= self.lr * (
-                    m_nesterov_b / (torch.sqrt(v_hat_b) + self.epsilon
-                ))
+                # Decoupled weight decay
+                if self.weight_decay > 0:
+                    layer.weight.data *= (1 - self.lr * self.weight_decay)
 
+                # Update
+                layer.weight.data -= self.lr * m_hat_w / (torch.sqrt(v_hat_w) + self.epsilon)
+                layer.bias.data -= self.lr * m_hat_b / (torch.sqrt(v_hat_b) + self.epsilon)
+
+class Nadam(Optimizer):
+    def __init__(self, layers, lr, beta1=0.9, beta2=0.999, epsilon=1e-8, weight_decay=0.0):
+        super().__init__(layers, lr, weight_decay)
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.epsilon = epsilon
+        self.t = 0
+        self.m = [{
+            'weights': torch.zeros_like(layer.weight.data),
+            'bias': torch.zeros_like(layer.bias.data)
+        } for layer in layers]
+        self.v = [{
+            'weights': torch.zeros_like(layer.weight.data),
+            'bias': torch.zeros_like(layer.bias.data)
+        } for layer in layers]
+
+    def step(self):
+        self.t += 1
+        with torch.no_grad():
+            for i, layer in enumerate(self.layers):
+                grad_w = layer.weight.grad
+                grad_b = layer.bias.grad
+
+                self.m[i]['weights'] = self.beta1 * self.m[i]['weights'] + (1 - self.beta1) * grad_w
+                self.m[i]['bias'] = self.beta1 * self.m[i]['bias'] + (1 - self.beta1) * grad_b
+
+                self.v[i]['weights'] = self.beta2 * self.v[i]['weights'] + (1 - self.beta2) * grad_w**2
+                self.v[i]['bias'] = self.beta2 * self.v[i]['bias'] + (1 - self.beta2) * grad_b**2
+
+                m_hat_w = self.m[i]['weights'] / (1 - self.beta1 ** self.t)
+                m_hat_b = self.m[i]['bias'] / (1 - self.beta1 ** self.t)
+                v_hat_w = self.v[i]['weights'] / (1 - self.beta2 ** self.t)
+                v_hat_b = self.v[i]['bias'] / (1 - self.beta2 ** self.t)
+
+                # Dozat-style m_bar (Nesterov acceleration)
+                m_bar_w = self.beta1 * m_hat_w + (1 - self.beta1) * grad_w
+                m_bar_b = self.beta1 * m_hat_b + (1 - self.beta1) * grad_b
+
+                if self.weight_decay > 0:
+                    layer.weight.data *= (1 - self.lr * self.weight_decay)
+
+                layer.weight.data -= self.lr * m_bar_w / (torch.sqrt(v_hat_w) + self.epsilon)
+                layer.bias.data -= self.lr * m_bar_b / (torch.sqrt(v_hat_b) + self.epsilon)
